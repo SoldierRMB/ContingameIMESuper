f9a42266d05360f165e96b346dafe32acd4fed37
SoldierRMB <soldierrmb0510@outlook.com>
2025-04-13 14:07:00 +0800

    Fix sign and hanging sign caret coordinate

--- fabric/src/main/java/city/windmill/ingameime/fabric/mixin/MixinEditScreen.java
+++ fabric/src/main/java/city/windmill/ingameime/fabric/mixin/MixinEditScreen.java
@@ -4,24 +4,27 @@ import city.windmill.ingameime.client.event.ClientScreenEventHooks;
 import kotlin.Pair;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.font.TextFieldHelper;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractSignEditScreen;
 import net.minecraft.client.gui.screens.inventory.BookEditScreen;
-import net.minecraft.client.gui.screens.inventory.HangingSignEditScreen;
 import net.minecraft.client.gui.screens.inventory.SignEditScreen;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.network.chat.Component;
 import net.minecraft.util.FormattedCharSequence;
+import net.minecraft.world.level.block.CeilingHangingSignBlock;
+import net.minecraft.world.level.block.WallHangingSignBlock;
 import net.minecraft.world.level.block.entity.SignBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import org.joml.Matrix4f;
+import org.jetbrains.annotations.Nullable;
+import org.joml.Vector3f;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-
-import java.lang.reflect.Field;
 
 @Mixin({Screen.class, AbstractSignEditScreen.class})
 class MixinScreen {
@@ -31,7 +34,7 @@ class MixinScreen {
     }
 }
 
-@Mixin({BookEditScreen.class, SignEditScreen.class})
+@Mixin({BookEditScreen.class, SignEditScreen.class, AbstractSignEditScreen.class})
 class MixinEditScreen {
     @Inject(method = "init", at = @At("TAIL"))
     private void onInit(CallbackInfo info) {
@@ -55,40 +58,78 @@ abstract class MixinBookEditScreen {
             at = @At(value = "INVOKE",
                     target = "Lnet/minecraft/client/gui/GuiGraphics;drawString(Lnet/minecraft/client/gui/Font;Lnet/minecraft/util/FormattedCharSequence;IIIZ)I"),
             locals = LocalCapture.CAPTURE_FAILSOFT)
-    private void onCaret_Book(GuiGraphics guiGraphics, int i, int j, float f, CallbackInfo ci, int k, FormattedCharSequence formattedCharSequence, int m, int n) {
+    private void onCaret_Book(GuiGraphics guiGraphics, int i, int j, float f, CallbackInfo ci, int k, int l, boolean bl, FormattedCharSequence formattedCharSequence, int m, int n) {
         ClientScreenEventHooks.INSTANCE.getEDIT_CARET().invoker().onEditCaret(this, new Pair<>(
-                k + 36 + (114 + n) / 2
-                        - Minecraft.getInstance().font.width("_"),
+                k + 36 + (114 - l) / 2 - Minecraft.getInstance().font.width("_"),
                 50
         ));
     }
 }
 
 @Mixin(AbstractSignEditScreen.class)
-abstract class MixinSignEditScreen extends Screen {
-    private MixinSignEditScreen(Component component) {
+abstract class MixinAbstractSignEditScreen extends Screen {
+    @Shadow
+    @Final
+    protected SignBlockEntity sign;
+
+    @Shadow
+    protected abstract Vector3f getSignTextScale();
+
+    @Shadow
+    private int line;
+    @Shadow
+    private @Nullable TextFieldHelper signField;
+    @Shadow
+    @Final
+    private String[] messages;
+
+    private MixinAbstractSignEditScreen(Component component) {
         super(component);
     }
 
-    @Inject(method = "renderSignText",
-            at = {
-                    @At(value = "INVOKE",
-                            target = "Lnet/minecraft/client/gui/GuiGraphics;drawString(Lnet/minecraft/client/gui/Font;Ljava/lang/String;IIIZ)I",
-                            ordinal = 1),
-                    @At(value = "INVOKE",
-                            target = "Lnet/minecraft/client/gui/GuiGraphics;fill(IIIII)V",
-                            ordinal = 0)},
-            locals = LocalCapture.CAPTURE_FAILSOFT)
-    private void onCaret_Sign(GuiGraphics guiGraphics, int i, int j, float f, CallbackInfo ci, float g, BlockState lv, boolean bl, boolean bl2, float h, MultiBufferSource.BufferSource lv2, float k, int l, int m, int n, int o, Matrix4f matrix4f, int p, String string, float q, int r, int s) {
-        try {
-            Field m03 = matrix4f.getClass().getDeclaredField("m03");
-            Field m13 = matrix4f.getClass().getDeclaredField("m13");
-            m03.setAccessible(true);
-            m13.setAccessible(true);
-            //s(23)->x,o(17)->y
-            ClientScreenEventHooks.INSTANCE.getEDIT_CARET().invoker().onEditCaret(this, new Pair<>((int) m03.get(matrix4f) + s, (int) m13.get(matrix4f) + o));
-        } catch (Exception ignored) {
+    @Inject(
+            method = "renderSignText",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/GuiGraphics;drawString(Lnet/minecraft/client/gui/Font;Ljava/lang/String;IIIZ)I",
+                    ordinal = 1
+            )
+    )
+    private void onCaret_Sign(GuiGraphics guiGraphics, CallbackInfo ci) {
+        if (signField == null) return;
 
-        }
+        String currentLine = messages[line];
+        int cursorPos = signField.getCursorPos();
+
+        assert minecraft != null;
+        int textWidth = minecraft.font.width(currentLine);
+        int horizontalOffset = minecraft.font.width(currentLine.substring(0, Math.min(cursorPos,
+                currentLine.length())))
+                - textWidth / 2;
+
+        Vector3f scale = getSignTextScale();
+        boolean isHanging = isHangingSign();
+
+        int screenX = (int) (horizontalOffset * scale.x()) + width / 2;
+        int screenY = calculateVerticalOffset(scale.y(), isHanging);
+
+        ClientScreenEventHooks.INSTANCE.getEDIT_CARET().invoker().onEditCaret(this, new Pair<>(screenX, screenY));
+    }
+
+    @Unique
+    private boolean isHangingSign() {
+        BlockState state = sign.getBlockState();
+        return state.getBlock() instanceof CeilingHangingSignBlock
+                || state.getBlock() instanceof WallHangingSignBlock;
+    }
+
+    @Unique
+    private int calculateVerticalOffset(float scaleY, boolean isHanging) {
+        int baseYOffset = isHanging ? 125 : 90;
+
+        int textLineHeight = sign.getTextLineHeight();
+        int verticalPos = line * textLineHeight - (4 * textLineHeight / 2);
+
+        return (int) (verticalPos * scaleY) + baseYOffset + (isHanging ? 2 : 0);
     }
 }
